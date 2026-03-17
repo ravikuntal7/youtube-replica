827698e3c31af0ed677c23952265f1cf5f667dfd
ravi <raviisamadeupname@gmail.com>
2026-03-17 15:36:32 +0530

    mongoose connection

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : "./env"
+})
+
+
+connectDB()
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+
+ 
+// ;( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/$(DB_NAME )`)
+//         app.on("error", (error) => {
+//             console.log("ERR" , err)
+
+//             throw err 
+
+            
+//         })
+
+//         app.listen(process.env.PORT,() =>{
+//             console.log(`process is listening on PORT ${process.env.PORT}`);
+//         })
+        
+//     } catch (error) {
+//         console.error("ERROR: " , error);
+//         throw err
+
+//     }
+// }) ()
